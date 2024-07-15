2c74304c95d0ef126e00349d27df460cc9155471
saugat110 <105158208+saugat110@users.noreply.github.com>
2024-07-15 21:24:08 +0545

    Initial commit

---
+++ templatetolaravel/resources/views/frontend/layout/header.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>Mexant - Financial HTML5 Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href=" {{asset('vendor/bootstrap/css/bootstrap.min.css')}} " rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="{{asset('css/fontawesome.css')}}">
+    <link rel="stylesheet" href="{{asset('css/templatemo-574-mexant.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.css')}}">
+    <link rel="stylesheet" href="{{asset('css/animate.css')}}">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+<!--
+
+TemplateMo 574 Mexant
+
+https://templatemo.com/tm-574-mexant
+
+-->
+  </head>
+
+<body>
+
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <nav class="main-nav">
+                      <!-- ***** Logo Start ***** -->
+                      <a href="index.html" class="logo">
+                          <img src="{{asset('images/logo.png')}} " alt="">
+                      </a>
+                      <!-- ***** Logo End ***** -->
+                      <!-- ***** Menu Start ***** -->
+                      <ul class="nav">
+                          <li class="scroll-to-section"><a href="{{url('/')}}" class="active">Home</a></li>
+                          <li class="scroll-to-section"><a href="#services">Services</a></li>
+                          <li class="scroll-to-section"><a href="#about">About</a></li>
+                          <li class="has-sub">
+                              <a href="javascript:void(0)">Pages</a>
+                              <ul class="sub-menu">
+                                  <li><a href="{{url('/aboutus')}}">About Us</a></li>
+                                  <li><a href="{{url('/ourservices')}}">Our Services</a></li>
+                                  <li><a href="{{url('/contactus')}}">Contact Us</a></li>
+                              </ul>
+                          </li>
+                          <li class="scroll-to-section"><a href="#testimonials">Testimonials</a></li>
+                          <li><a href="{{url('/contactus')}}">Contact Support</a></li> 
+                      </ul>        
+                      <a class='menu-trigger'>
+                          <span>Menu</span>
+                      </a>
+                      <!-- ***** Menu End ***** -->
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
--- templatetolaravel/routes/web.php
+++ templatetolaravel/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Frontend\AboutusController;
+use App\Http\Controllers\Frontend\ContactusController;
+use App\Http\Controllers\Frontend\OurservicesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
+
+Route::get('/aboutus', [AboutusController::class, 'index']);
+
+Route::get('/contactus', [ContactusController::class, 'index']);
+
+Route::get('/ourservices', [OurservicesController::class, 'index']);
+
